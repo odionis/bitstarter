816bbfe39efbaf09f2e4940d2c0c9e2434734568
osdiomar <odionis@gmail.com>
2013-07-10 22:11:06 +0200

    Modification to complete homework 3, part 1

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+var buff = new Buffer(27);
+
+buff.write(fs.readFileSync('index.html', 'utf-8'), "utf-8");
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(request, response) 
+{
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+app.listen(port, function() 
+{
   console.log("Listening on " + port);
 });
