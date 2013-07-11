7d9866b26088846b5cbdfb336e81a30255a77b30
osdiomar <odionis@gmail.com>
2013-07-11 20:15:02 +0200

    Added checks.json and grader.js to complete hw3 part 3

--- grader.js
+++ grader.js
@@ -26,6 +26,9 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var sys = require('util');
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,11 +64,24 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Url to analize')
         .parse(process.argv);
+    if (program.url)
+    {
+      console.log ("hola");
+      rest.get('http://google.com').on('complete', function(result) {
+        if (result instanceof Error) {
+          sys.puts('Error: ' + result.message);
+          this.retry(5000); // try again after 5 sec
+        }
+      });
+    }
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
